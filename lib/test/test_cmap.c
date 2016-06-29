d90bdc35ed5f42baafd8bf9dce9aafbecca12899
lorenzod <lorenzod@plumgrid.com>
2016-06-29 22:16:28 +0000

    Raw porting of openvswitch cmap
    Signed-off-by: lorenzod <lorenzod@plumgrid.com>

---
+++ lib/test/test_cmap.c
@@ -0,0 +1,49 @@
+/*
+ * =====================================================================
+ * Copyright (c) 2016 PLUMgrid, http://plumgrid.com
+ *
+ * This source is subject to the PLUMgrid License.
+ * All rights reserved.
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF
+ * ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ *
+ * PLUMgrid confidential information, delete if you are not the
+ * intended recipient.
+ *
+ * =====================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lib/cmap.h"
+
+typedef struct my_node {
+  struct cmap_node cmap_node;
+  int data;
+} my_node;
+
+int main() {
+  struct cmap cmap;
+  struct my_node my_node;
+  struct my_node my_node_2;
+  struct my_node * iterator;
+  struct cmap_node *cmap_node;
+  uint32_t hash = 42;
+
+  my_node.data = 24;
+  my_node_2.data = 23;
+
+  cmap_init(&cmap);
+  cmap_insert(&cmap, &(my_node.cmap_node), hash);
+  cmap_insert(&cmap, &(my_node_2.cmap_node), hash);
+
+  CMAP_FOR_EACH_WITH_HASH(iterator, cmap_node, hash, &cmap) {
+    printf("Node data: %d\n", iterator->data);
+  }
+
+  return 0;
+}
